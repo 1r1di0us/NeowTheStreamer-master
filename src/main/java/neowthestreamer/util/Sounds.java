9ce9527ac34ebe4651f840728175c21ce7043b17
1r1di0us <57327873+1r1di0us@users.noreply.github.com>
2026-01-12 15:57:34 -0700

    Initial commit!!!

---
+++ src/main/java/neowthestreamer/util/Sounds.java
@@ -0,0 +1,8 @@
+package neowthestreamer.util;
+
+import static neowthestreamer.NeowTheStreamer.audioPath;
+
+public class Sounds {
+    public static String TEST_SOUND = audioPath("test.wav"); //Load audio using a given path
+    public static String ding; //Load audio from audioPath based on the field name
+}
